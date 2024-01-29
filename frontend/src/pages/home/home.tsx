d2790bd803dfd2979bd62b5b06ff26a01130a69c
mak9036 <marcusdenseje123@gmail.com>
2024-01-29 12:13:51 +0100

    Oplev tilføjet

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,11 +1,12 @@
 import './home.css'
 import Tilbyder from '../../components/tilbyder/tilbyder'
-
+import Oplev from '../../components/oplevelse/oplev'
 function Home() {
 
   return (
     <>
     <Tilbyder/>
+    <Oplev/>
     </>
   )
 }
