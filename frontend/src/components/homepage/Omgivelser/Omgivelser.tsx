2b8c6689b67a937e6dc2052a5ccc061094aabfc3
StiggitS <142784487+StiggitS@users.noreply.github.com>
2024-01-29 12:15:11 +0100

    Added components for Vaerelser & Omgivelser.

---
+++ frontend/src/components/homepage/Omgivelser/Omgivelser.tsx
@@ -0,0 +1,11 @@
+import styles from './Omgivelser.module.css'
+
+const Omgivelser = () => {
+  return (
+    <div>
+      Omgivelser
+    </div>
+  )
+}
+
+export default Omgivelser
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,12 +1,14 @@
 import './home.css'
 import Tilbyder from '../../components/tilbyder/tilbyder'
 import Hero from '../../components/homepage/hero/Hero.tsx'
+import Omgivelser from '../../components/homepage/Omgivelser/Omgivelser.tsx'
 
 function Home() {
 
   return (
     <>
     <Hero />
+    <Omgivelser />
     <Tilbyder/>
     </>
   )
