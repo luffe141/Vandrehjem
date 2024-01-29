a7f8ce754a3c3ef8891bb533a79a3eeb34262fc1
StiggitS <142784487+StiggitS@users.noreply.github.com>
2024-01-29 13:26:04 +0100

    fixed home & navbar needs work!

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -3,6 +3,7 @@ import Tilbyder from '../../components/tilbyder/tilbyder'
 import Hero from '../../components/homepage/hero/Hero.tsx'
 import Omgivelser from '../../components/homepage/Omgivelser/Omgivelser.tsx'
 import Rooms from '../../components/homepage/Rooms/Rooms.tsx'
+import Oplev from '../../components/oplevelse/oplev.tsx'
 
 function Home() {
 
