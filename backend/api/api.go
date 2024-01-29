4c2fa64529a30a9521a4469b28e58d3821ece992
Lars M Bek <lars@beksoft.dk>
2024-01-29 12:36:00 +0100

    Added unfinished work (routing)

--- backend/api/api.go
+++ backend/api/api.go
@@ -6,10 +6,18 @@ func AddRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	// routes
-	mux.HandleFunc("GET /users/", handleGetUsers)
-	mux.HandleFunc("GET /users/{id}/", handleGetUsersId)
-	mux.HandleFunc("GET /usersa/", handleGetUsersa)
-	mux.HandleFunc("GET /usersb/", handleGetUsersb)
+	//mux.HandleFunc("GET /users/", handleGetUsers)
+	//mux.HandleFunc("POST /users/", handlePostUsers)
+	//mux.HandleFunc("GET /users/{id}/", handleGetUsersId)
+
+	//mux.HandleFunc("GET /usersa/", handleGetUsersa)
+	//mux.HandleFunc("GET /usersb/", handleGetUsersb)
+
+	//mux.HandleFunc("GET /users/", tasks.GetAll)
+	//mux.HandleFunc("GET /users/{id}/", tasks.Get)
+	mux.HandleFunc("POST /users/", handlePostUsers)
+	//mux.HandleFunc("PUT /users/{id}/", tasks.Put)
+	//mux.HandleFunc("DELETE /users/{id}/", tasks.Delete)
 
 	return mux
 }
--- backend/api/handlers.go
+++ backend/api/handlers.go
@@ -1,12 +1,43 @@
 package api
 
 import (
+	"backend/database/mongodb"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"net/http"
 )
 
+type Users struct {
+	Name string
+	Age  int
+}
+
 func handleGetUsers(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(response, "her er alle brugerne 1")
+	store := mongodb.NewStorage("mongodb://localhost:27017", "Vandrerhjem", "Users", "name")
+	defer store.Close()
+
+	data, err := store.ReadData(bson.M{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("hej med dig")
+	fmt.Println(data)
+	_, _ = fmt.Fprint(response, data)
+}
+
+func handlePostUsers(response http.ResponseWriter, request *http.Request) {
+	store := mongodb.NewStorage("mongodb://localhost:27017", "Vandrerhjem", "Users", "name")
+	defer store.Close()
+
+	// Create
+	err := store.CreateData(Users{Name: "Lars", Age: 29})
+	if err != nil {
+		_, _ = fmt.Fprint(response, err)
+	} else {
+		_, _ = fmt.Fprint(response, "you have succesfully added a new user")
+	}
+
 }
 
 func handleGetUsersa(response http.ResponseWriter, request *http.Request) {
