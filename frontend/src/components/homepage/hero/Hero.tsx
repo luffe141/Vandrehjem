83049be06c95e2686a35b777988aa20b29e9df74
StiggitS <142784487+StiggitS@users.noreply.github.com>
2024-01-29 10:40:37 +0100

    added header stuff and started hero component

---
+++ frontend/src/components/homepage/hero/Hero.tsx
@@ -0,0 +1,9 @@
+import styles from './Hero.module.css'
+
+const Hero = () => {
+  return (
+    <div id={styles.heroDiv}>hero component</div>
+  )
+}
+
+export default Hero
--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,10 +1,14 @@
 import './home.css'
+import Hero from '../../components/homepage/hero/Hero.tsx'
 
 function Home() {
 
   return (
     <>
-
+      <div>
+        <p>test Home component</p>
+      </div>
+      <Hero />
     </>
   )
 }
