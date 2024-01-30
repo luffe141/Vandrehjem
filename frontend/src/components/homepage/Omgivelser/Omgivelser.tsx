615eba9554122dc6addf62e17a45882d8f908d42
StiggitS <142784487+StiggitS@users.noreply.github.com>
2024-01-30 09:27:57 +0100

    changed and added styling to img gallery/slider

--- frontend/src/components/homepage/Omgivelser/Omgivelser.tsx
+++ frontend/src/components/homepage/Omgivelser/Omgivelser.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import styles from './Omgivelser.module.css'
+import ImageSlider from './ImageSlider';
+
+
+
 
 const Omgivelser = () => {
-  return (
-    <div>
-      Omgivelser
-    </div>
-  )
-}
+  const images = [
+    'https://picsum.photos/id/237/300/300',
+    'https://picsum.photos/id/49/300/300',
+    'https://picsum.photos/id/159/300/300',
+  ];
 
-export default Omgivelser
+  return (
+    <div className={styles.OmgivelserDiv}>
+      <h1>Image Slider App</h1>
+      <ImageSlider images={images} />
+    </div>
+  );
+};
+
+export default Omgivelser;
--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,40 +1,12 @@
-import { useState } from "react";
 import styles from "./navbar.module.css";
-<link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-></link>;
+
 
 function Navbar() {
-  const [isLinksVisible, setIsLinksVisible] = useState(true);
-
-  const toggleLinks = () => {
-    setIsLinksVisible(!isLinksVisible);
-  };
+  
 
   return (
     <nav>
-		
-      <div className={styles.topnav}>
-        <a href="#home" className={styles.active}>
-          Logo
-        </a>
-
-        {/*<!-- Navigation links (hidden by default) -->*/}
-        <div
-          id="myLinks"
-          style={{ display: isLinksVisible ? "block" : "none" }}>
-          <a href="#">test 1</a>
-          <a href="#">test 2</a>
-        </div>
-
-        {/*<!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->*/}
-        <button onClick={toggleLinks} className={styles.icon}>
-          Toggle Links
-        </button>
-        <i className="fa fa-bars"></i>
-      </div>
-
+		<p className={styles.navP}>navbaren her</p>
     </nav>
   );
 }
