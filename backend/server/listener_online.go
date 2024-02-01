3f214b350c4dff447ffd0245c06b1ccb4e07f207
Lars M Bek <lars@beksoft.dk>
2024-02-01 10:46:35 +0100

    Commit message: Add comment to Listen function in listener_online.go
    Added an explanatory comment to the Listen function in 'listener_online.go'. This improves code comprehension, providing a brief description of what the function does: start an HTTP server and listen on specified address and port.

--- backend/server/listener_online.go
+++ backend/server/listener_online.go
@@ -4,6 +4,7 @@ package server
 
 import "net/http"
 
+// Listen starts the HTTP server and listens on the specified address and port.
 func (server webserver) Listen() error {
 	return http.ListenAndServeTLS(server.addr+":"+server.port, "path to certificate file", mux)
 }
