5baf40a91f82031c3b8a3c620fbee64ca1abce30
Lars M Bek <lars@beksoft.dk>
2024-02-01 10:44:31 +0100

    Add CRUD operations in activities controller
    The activities controller now includes the functions PostActivity, PutActivity, and DeleteActivity. These functions enable create, update, and delete operations respectively for activities using MongoDB storage.

--- backend/api/controllers/activities.go
+++ backend/api/controllers/activities.go
@@ -28,3 +28,45 @@ func GetActivity(id string) (any, error) {
 
 	return store.ReadData(filter)
 }
+
+func PostActivity(data any) error {
+	store := mongodb.NewStorage("mongodb://localhost:27017", databaseName, activityCollection, "name")
+	defer store.Close()
+
+	return store.CreateData(data)
+}
+
+func PutActivity(id string, data any) error {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	store := mongodb.NewStorage("mongodb://localhost:27017", databaseName, activityCollection, "name")
+	defer store.Close()
+
+	filter := bson.M{
+		"_id": objectID,
+	}
+
+	updateData := bson.M{
+		"$set": data,
+	}
+
+	return store.UpdateData(filter, updateData)
+}
+
+func DeleteActivity(id string) error {
+	store := mongodb.NewStorage("mongodb://localhost:27017", databaseName, activityCollection, "name")
+	defer store.Close()
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{
+		"_id": objectID,
+	}
+
+	return store.DeleteData(filter)
+}
