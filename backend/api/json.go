7a55cbbfa0a4d74c64a02015b368fbeba8184ace
Lars Morten Bek <lars@beksoft.dk>
2024-02-02 07:41:32 +0100

    Refactor API response handling in jsonWrapper
    Introduced specific response structures for error and success scenarios, thus enhancing the readability and detailed nature of the responses. Updated the jsonWrapper function to use these new structures, consequently improving error handling and the structure of successful responses. This refactoring will provide a more consistent, comprehensive API response structure.

--- backend/api/json.go
+++ backend/api/json.go
@@ -5,21 +5,39 @@ import (
 	"net/http"
 )
 
-func jsonWrapper(handler func(http.ResponseWriter, *http.Request) (any, error)) http.HandlerFunc {
+type ErrorResponse struct {
+	Message string `json:"message"`
+	Error   string `json:"error"`
+}
+
+type SuccessResponse struct {
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func JsonWrapper(handler func(http.ResponseWriter, *http.Request) (interface{}, error)) http.HandlerFunc {
 	return func(response http.ResponseWriter, request *http.Request) {
-		// Call the handler function which should return your data and error
 		data, err := handler(response, request)
-		if err != nil {
-			http.Error(response, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		// Set the output content type to JSON
+
 		response.Header().Set("Content-Type", "application/json")
-		// Convert and write your data as JSON to the response
-		err = json.NewEncoder(response).Encode(data)
+
 		if err != nil {
-			http.Error(response, err.Error(), http.StatusInternalServerError)
+			errResponse := ErrorResponse{Message: "An error occurred processing your request", Error: err.Error()}
+			jsonErrData, _ := json.Marshal(errResponse)
+			http.Error(response, string(jsonErrData), http.StatusInternalServerError)
 			return
 		}
+
+		successResponse := SuccessResponse{Message: "Request processed successfully", Data: data}
+		jsonSuccessData, err := json.Marshal(successResponse)
+
+		if err != nil {
+			errResponse := ErrorResponse{Message: "An error occurred processing your request", Error: err.Error()}
+			jsonErrData, _ := json.Marshal(errResponse)
+			http.Error(response, string(jsonErrData), http.StatusInternalServerError)
+			return
+		}
+
+		response.Write(jsonSuccessData)
 	}
 }
