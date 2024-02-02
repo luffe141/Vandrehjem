e26ed56496ee3f63fe6f050c6567fb211d6388e2
StiggitS <142784487+StiggitS@users.noreply.github.com>
2024-02-02 10:29:49 +0100

    Galleri page soon ready for data from server.

--- frontend/src/components/Galleri/Galleri.tsx
+++ frontend/src/components/Galleri/Galleri.tsx
@@ -1,13 +1,14 @@
-import styles from './Galleri.module.css'
+import styles from "./Galleri.module.css";
 
 const Galleri = () => {
   return (
+    // Images hentes og vises fra serveren via api
     <>
-    <div className={styles.galleriContainer}>
-    
-    </div>
+      <div className={styles.galleriContainer}>
+        
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Galleri
+export default Galleri;
