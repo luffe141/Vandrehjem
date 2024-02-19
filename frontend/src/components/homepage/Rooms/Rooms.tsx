8b1ec15f734d6f7e268f502aae5533c5f4f9d8ab
Lars M Bek <lars@beksoft.dk>
2024-02-19 09:03:53 +0100

    Modified

--- frontend/src/components/homepage/Rooms/Rooms.tsx
+++ frontend/src/components/homepage/Rooms/Rooms.tsx
@@ -1,36 +1,65 @@
+import React from "react";
 import styles from "./Rooms.module.css";
 
 const Rooms = () => {
+  const images: string[] = [
+    "http://localhost:5173/Img/Billeder/Billeder/Vandrerhjem/vaerelse1.jpg",
+    "http://localhost:5173/Img/Billeder/Billeder/Vandrerhjem/vaerelse2.jpg",
+    "http://localhost:5173/Img/Billeder/Billeder/Vandrerhjem/danhostel-gjerrild.jpg",
+    "http://localhost:5173/Img/Billeder/Billeder/Vandrerhjem/danhostel-gjerrild.jpg",
+  ];
   return (
-
-    <div>
-      <h1 id={styles.roomsTitle}>Rooms</h1>
-      <div className={styles.container}>
-          <div className={styles.roomImgDiv}>
-            <div className={styles.topImgContainer}>
-              <img src="https://placehold.jp/250x150.png" alt="img_1" />
-              <img src="https://placehold.jp/100x100.png" alt="img_2" />
+      <div className={styles.bigContainer}>
+        <h1 className={styles.roomH1}>Rooms</h1>
+        <div className={styles.container}>
+          <div className={styles.roomDiv}>
+            <div className={styles.roomImgDiv}>
+              <div className={styles.topImgContainer}>
+                <div className={styles.bottomImgContainer}>
+                  <img
+                      className={styles.bottomLeftImg}
+                      src={images[2]}
+                      alt="Room Image 3"
+                  />
+                  <img
+                      className={styles.bottomRightImg}
+                      src={images[3]}
+                      alt="Room Image 4"
+                  />
+                </div>
+                <div className={styles.bottomImgContainer}>
+                  <img
+                      className={styles.topLeftImg}
+                      src={images[0]}
+                      alt="Room Image 1"
+                  />
+                  <img
+                      className={styles.topRightImg}
+                      src={images[1]}
+                      alt="Room Image 2"
+                  />
+                </div>
+              </div>
             </div>
-            <div className={styles.bottomImgContainer}>
-              <img src="https://placehold.jp/100x100.png" alt="img_3" />
-              <img src="https://placehold.jp/100x100.png" alt="img_4" />
+            <div className={styles.roomInfo}>
+              <h2>1-4 personers værelse</h2>
+              <p className={styles.roomText}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+                deserunt sit non quidem tenetur nihil nisi reiciendis corporis
+                eius! Porro maxime adipisci perferendis cum debitis facere facilis
+                quisquam repellat tempore.
+              </p>
+              <label htmlFor="">Pris,-</label>
+              <button>Book nu</button>
             </div>
           </div>
-        <div className={styles.roomInfo}>
-          <h2>1-4 personers værelse</h2>
-          <p>
-            Lorem ipsum deez nuts sit amet consectetur adipisicing elit. Omnis
-            deserunt sit non quidem tenetur nihil nisi reiciendis corporis eius!
-            Porro maxime adipisci perferendis cum debitis facere facilis
-            quisquam repellat tempore.
-          </p>
-          <label htmlFor="">Pris,- 495,00</label>
-          <button><a href="/Vaerelser">Book nu</a></button>
         </div>
       </div>
-    </div>
   );
 };
 
+<div className={styles.roomDiv}>
+  <h1>Rooms</h1>
+</div>;
 
 export default Rooms;
